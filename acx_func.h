buraktutanlar/acx100-acx-mac80211

commit 9a2483499c2923e493d00b5c9818dad7f88f5ffb

merge parts of the patch from: https://dev.openwrt.org/browser/trunk/package/acx-mac80211/patches/003-mac80211_fixes.diff

acx_func.h
@@ -625,7 +625,20 @@ int acx_net_conf_tx(struct ieee80211_hw* ieee, int queue,
 int acxpci_s_reset_dev(acx_device_t *adev);
 void acx_e_after_interrupt_task(struct work_struct* work);
 void acx_i_set_multicast_list(struct ieee80211_hw *hw,
-                            unsigned short netflags, int mc_count);
+                            unsigned int changed_flags,
+                            unsigned int *total_flags,
+                            int mc_count, struct dev_addr_list *mc_list);
+
+enum ieee80211_filter_flags {
+FIF_PROMISC_IN_BSS,
+FIF_ALLMULTI,
+};
+
+enum ieee80211_key_flags {
+IEEE80211_KEY_FLAG_WMM_STA,
+IEEE80211_KEY_FLAG_GENERATE_IV,
+IEEE80211_KEY_FLAG_GENERATE_MMIC
+};
 
 /*** End DeviceScape Functions **/
 

common.c
@@ -1704,7 +1704,9 @@ void acx_s_cmd_join_bssid(acx_device_t *adev, const u8 *bssid)
 */
 void
 acx_i_set_multicast_list(struct ieee80211_hw *hw,
-                            unsigned short netflags, int mc_count)
+                         unsigned int changed_flags,
+                         unsigned int *total_flags,
+                         int mc_count, struct dev_addr_list *mc_list)
 {
         acx_device_t *adev = ieee2adev(hw);
         unsigned long flags;
@@ -1713,9 +1715,12 @@ acx_i_set_multicast_list(struct ieee80211_hw *hw,
 
         acx_lock(adev, flags);
 
+        *total_flags &= (FIF_PROMISC_IN_BSS | FIF_ALLMULTI);
+        if ((changed_flags & (FIF_PROMISC_IN_BSS | FIF_ALLMULTI)) == 0)
+                return;
         /* firmwares don't have allmulti capability,
          * so just use promiscuous mode instead in this case. */
-        if (netflags & (IFF_PROMISC | IFF_ALLMULTI)) {
+        if (*total_flags) {
                 SET_BIT(adev->rx_config_1, RX_CFG1_RCV_PROMISCUOUS);
                 CLEAR_BIT(adev->rx_config_1, RX_CFG1_FILTER_ALL_MULTI);
                 SET_BIT(adev->set_mask, SET_RXCONFIG);
@@ -4562,7 +4567,7 @@ int acx_key_write(acx_device_t * adev,
 */
 
 int acx_net_set_key(struct ieee80211_hw *ieee,
-		    set_key_cmd cmd,
+		    set_key_cmd cmd, 
 		    u8 * addr, struct ieee80211_key_conf *key, int aid)
 {
 //      return 0;
@@ -4575,11 +4580,11 @@ int acx_net_set_key(struct ieee80211_hw *ieee,
 //	TODO();
 	switch (key->alg) {
 	default:
-	case ALG_NONE:
+/*	case ALG_NONE:
 	case ALG_NULL:
 		algorithm = ACX_SEC_ALGO_NONE;
 		break;
-	case ALG_WEP:
+*/	case ALG_WEP:
 		if (key->keylen == 5)
 			algorithm = ACX_SEC_ALGO_WEP;
 		else
@@ -4604,20 +4609,21 @@ int acx_net_set_key(struct ieee80211_hw *ieee,
 		if (err)
 			goto out_unlock;
 		key->hw_key_idx = index;
-		CLEAR_BIT(key->flags, IEEE80211_KEY_FORCE_SW_ENCRYPT);
-		if (CHECK_BIT(key->flags, IEEE80211_KEY_DEFAULT_TX_KEY))
-			adev->default_key_idx = index;
+/*		CLEAR_BIT(key->flags, IEEE80211_KEY_FORCE_SW_ENCRYPT);*/
+/*		if (CHECK_BIT(key->flags, IEEE80211_KEY_DEFAULT_TX_KEY))
+			adev->default_key_idx = index;*/
+                SET_BIT(key->flags, IEEE80211_KEY_FLAG_GENERATE_IV);
 		adev->key[index].enabled = 1;
 		break;
 	case DISABLE_KEY:
 		adev->key[index].enabled = 0;
 		err = 0;
 		break;
-	case REMOVE_ALL_KEYS:
+/*	case REMOVE_ALL_KEYS:
 		acx_clear_keys(adev);
 		err = 0;
 		break;
-     /* case ENABLE_COMPRESSION:
+*/    /* case ENABLE_COMPRESSION:
 	case DISABLE_COMPRESSION:
 		err = 0;
 		break; */

pci.c
@@ -83,7 +83,7 @@ static irqreturn_t acxpci_i_interrupt(int irq, void *dev_id);
 static void disable_acx_irq(acx_device_t * adev);
 
 static int acxpci_e_open(struct ieee80211_hw *hw);
-static int acxpci_e_close(struct ieee80211_hw *hw);
+static void acxpci_e_close(struct ieee80211_hw *hw);
 static void acxpci_s_up(struct ieee80211_hw *hw);
 static void acxpci_s_down(struct ieee80211_hw *hw);
 
@@ -1440,7 +1440,7 @@ static const struct ieee80211_ops acxpci_hw_ops = {
         .remove_interface = acx_remove_interface,
 	.open = acxpci_e_open,
 	.stop = acxpci_e_close,
-        .reset = acx_net_reset,
+/*        .reset = acx_net_reset,*/
         .config = acx_net_config,
         .config_interface = acx_config_interface,
         .set_multicast_list = acx_i_set_multicast_list,
@@ -1477,9 +1477,7 @@ acxpci_e_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 		       pci_name(pdev));
 		goto fail_alloc_netdev;
 	}
-	ieee->flags &=	 ~IEEE80211_HW_RX_INCLUDES_FCS &
-			 ~IEEE80211_HW_MONITOR_DURING_OPER |
-			 IEEE80211_HW_WEP_INCLUDE_IV;
+	ieee->flags &=	 ~IEEE80211_HW_RX_INCLUDES_FCS;
 	ieee->queues = 1;
 
 	/* (NB: memsets to 0 entire area) */
@@ -2135,7 +2133,7 @@ static int acxpci_e_open(struct ieee80211_hw *hw)
 **	>0	f/w reported error
 **	<0	driver reported error
 */
-static int acxpci_e_close(struct ieee80211_hw *hw)
+static void acxpci_e_close(struct ieee80211_hw *hw)
 {
 	acx_device_t *adev = ieee2adev(hw);
 	unsigned long flags;
@@ -2164,7 +2162,6 @@ static int acxpci_e_close(struct ieee80211_hw *hw)
 
 	log(L_INIT, "closed device\n");
 	FN_EXIT0;
-	return OK;
 }
 
 

commit af55340781f32991d67d31915742c0d7c3b65239

Added proper fix for a possible bug introduced by the patch in commit 9a2483499c2923e493d00b5c9818dad7f88f5ffb

acx_func.h
@@ -639,9 +639,6 @@ void acx_e_after_interrupt_task(struct work_struct* work);
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,24)
 void acx_i_set_multicast_list(struct ieee80211_hw *hw,
                             unsigned short netflags, int mc_count);
-enum ieee80211_key_flags {
-IEEE80211_KEY_FLAG_GENERATE_IV,
-};
 #else
 void acx_i_set_multicast_list(struct ieee80211_hw *hw,
                             unsigned int changed_flags,

common.c
@@ -4652,7 +4652,10 @@ int acx_net_set_key(struct ieee80211_hw *ieee,
 /*		CLEAR_BIT(key->flags, IEEE80211_KEY_FORCE_SW_ENCRYPT);*/
 /*		if (CHECK_BIT(key->flags, IEEE80211_KEY_DEFAULT_TX_KEY))
 			adev->default_key_idx = index;*/
+		#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,24)
                 SET_BIT(key->flags, IEEE80211_KEY_FLAG_GENERATE_IV);
+		#else
+		#endif
 		adev->key[index].enabled = 1;
 		break;
 	case DISABLE_KEY: