0f1378d895ee0edf5175525fddf74a94ad7ce189
XazZ <devel@xazz.no-ip.info>
2008-01-20 19:13:32 +0100

    modinfo acx-mac80211.ko now shows the driver version

--- common.c
+++ common.c
@@ -55,6 +55,8 @@ MODULE_AUTHOR("ACX100 Open Source Driver development team");
 MODULE_DESCRIPTION
     ("Driver for TI ACX1xx based wireless cards (CardBus/PCI/USB)");
 
+MODULE_VERSION(ACX_RELEASE);
+
 
 /***********************************************************************
 */
