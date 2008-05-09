2dffcdcaaa175dbc2a770e278914318d336ea04d
Francis Galiegue <fgaliegue@gmail.com>
2008-05-10 01:45:51 +0200

    log.c: fix missing va_end() :(
    Talk about stupid bugs :(
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- log.c
+++ log.c
@@ -108,6 +108,7 @@ void acx_log_dump(int level, int what, const void *buf, ssize_t buflen,
 	va_start(args, fmt);
 	
 	acx_log(level, what, fmt, args);
+	va_end(args);
 	acx_dump_bytes(printk_level, buf, buflen);
 }
 /**
