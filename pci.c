ea60fdb09963ee92a3aefb08ecba13dea38c231c
Oliver Winker <oliver@oli1170.net>
2010-06-14 18:06:00 +0200

    pci: Move acxpci_log_irq() to common
    Signed-off-by: Oliver Winker <oliver@oli1170.net>

--- pci.c
+++ pci.c
@@ -145,7 +145,6 @@ static void acxpci_irq_disable(acx_device_t * adev);
 void acxpci_irq_work(struct work_struct *work);
 static irqreturn_t acxpci_i_interrupt(int irq, void *dev_id);
 static void acxpci_handle_info_irq(acx_device_t * adev);
-static void acxpci_log_irq(u16 irqtype);
 void acxpci_set_interrupt_mask(acx_device_t * adev);
 
 // Mac80211 Ops
@@ -2916,62 +2915,6 @@ static void acxpci_handle_info_irq(acx_device_t * adev)
 	    info_type_msg[(info_type >= ARRAY_SIZE(info_type_msg)) ?
 			  0 : info_type]
 	    );
-}
-
-
-static void acxpci_log_irq(u16 irqtype)
-{
-	printk("acxpci: %s: got: ", __func__);
-
-	if (irqtype & HOST_INT_RX_DATA)
-		printk("Rx_Data,");
-
-	if (irqtype & HOST_INT_TX_COMPLETE)
-		printk("Tx_Complete,");
-
-	if (irqtype & HOST_INT_TX_XFER)
-		printk("Tx_Xfer,");
-
-	if (irqtype & HOST_INT_RX_COMPLETE)
-		printk("Rx_Complete,");
-
-	if (irqtype & HOST_INT_DTIM)
-		printk("DTIM,");
-
-	if (irqtype & HOST_INT_BEACON)
-		printk("Beacon,");
-
-	if (irqtype & HOST_INT_TIMER)
-		log(L_IRQ, "Timer,");
-
-	if (irqtype & HOST_INT_KEY_NOT_FOUND)
-		printk("Key_Not_Found,");
-
-	if (irqtype & HOST_INT_IV_ICV_FAILURE)
-		printk("IV_ICV_Failure (crypto),");
-
-	if (irqtype & HOST_INT_CMD_COMPLETE)
-		printk("Cmd_Complete,");
-
-	if (irqtype & HOST_INT_INFO)
-		printk("Info,");
-
-	if (irqtype & HOST_INT_OVERFLOW)
-		printk("Overflow,");
-
-	if (irqtype & HOST_INT_PROCESS_ERROR)
-		printk("Process_Error,");
-
-	if (irqtype & HOST_INT_SCAN_COMPLETE)
-		printk("Scan_Complete,");
-
-	if (irqtype & HOST_INT_FCS_THRESHOLD)
-		printk("FCS_Threshold,");
-
-	if (irqtype & HOST_INT_UNKNOWN)
-		printk("Unknown,");
-
-	printk(": IRQ(s)\n");
 }
 
 void acxpci_set_interrupt_mask(acx_device_t * adev)
