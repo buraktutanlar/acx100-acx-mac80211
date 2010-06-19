8710ce1cf4a4e553d2f4f129d85fb75f9061c31d
Oliver Winker <oliver@oli1170.net>
2010-06-19 19:52:08 +0200

    common: acx_e_after_interrupt_task(): Logging CMD_RADIO_RECALIB
    Signed-off-by: Oliver Winker <oliver@oli1170.net>

--- common.c
+++ common.c
@@ -5080,7 +5080,7 @@ void acx_e_after_interrupt_task(acx_device_t *adev)
 
 	/* we see lotsa tx errors */
 	if (adev->after_interrupt_jobs & ACX_AFTER_IRQ_CMD_RADIO_RECALIB) {
-		logf0(L_ANY, "Schedule CMD_RADIO_RECALIB\n");
+		logf0(L_DEBUG, "Schedule CMD_RADIO_RECALIB\n");
 		acx_s_after_interrupt_recalib(adev);
 	}
 
